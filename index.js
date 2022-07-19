81dc13067fd5699e6d17e82607b7894f450c95fd
King Sonkorey <sam.kimotho450@gmail.com>
2022-07-19 19:11:16 +0300

    updated

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ function computerPlay(){
             return "Invalid input";
         } else if (playerSelection !== "rock" && playerSelection !== "scissors" && playerSelection !== "paper") {
             return "Invalid input";
-        } else if (playerSelection === computerSelection) {
-            return `It's a tie! you both picked ${playerSelection}`;
+        } else if (playerSelection  === computerSelection) {
+            return `It's a tie! ${playerSelection}you both picked ${computerSelection}`;
         } else if (playerSelection === "rock" && computerSelection === "scissors") {
             return "You win! Rock beats Scissors";
+        }else if (playerSelection === "scissors" && computerSelection === "rock") {
+                return "You lose! Rock beats Scissors";
         } else if (playerSelection === "paper" && computerSelection === "rock") {
             return "You win! Paper beats Rock";
+        }else if (playerSelection === "rock" && computerSelection === "paper") {
+                return "You lose! Paper beats Rock";
         } else if (playerSelection === "scissors" && computerSelection === "paper") {
             return "You win! Scissors beats Paper";
-        } else if  (playerSelection && computerSelection){
-            return `You lose! ${playerSelection} beats ${computerSelection}`;
+        }else if (playerSelection === "paper" && computerSelection === "scissors") {
+                return "You lose! Scissors beats Paper";
+        } else if  (playerSelection === computerSelection){
+            return `You lose! ${computerSelection} beats ${playerSelection} `;
         }else{
             return `You win! ${playerSelection} beats ${computerSelection}`;
         }
