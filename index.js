0a113080a0db800c761b1a1d022477c195bc504a
King Sonkorey <sam.kimotho450@gmail.com>
2022-07-21 18:28:56 +0300

    changes

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 function computerPlay(){
     const start = ['Rock','Paper','Scissors'];
     return start[Math.floor(Math.random()*start.length)];
-    } 
-    function validInput(userInput){
-        userInput = userInput.toLowerCase();
-        if(userInput === "rock"  && userInput === "scissors" && userInput === "paper"){
-            return true;
-        }else{
-            return false;
-        }
-    };
+} 
+// function validInput(userInput){
+//     userInput = userInput.toLowerCase();
+//     if(userInput === "rock"  && userInput === "scissors" && userInput === "paper"){
+//     return true;
+//     }else{
+//     return false;
+//     }
+// };
 function playRound(playerSelection, computerSelection){
-        if (playerSelection == null || computerSelection == null) {
-            return "Invalid input";
+    if (playerSelection == null || computerSelection == null) {
+        return "Invalid input";
+    }
+    if (playerSelection === computerSelection) {
+        return "It's a Tie";
+    }
+    if (playerSelection !== "rock" && playerSelection !== "scissors" && playerSelection !== "paper") {
+        return "Invalid input";
+    }
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Scissors") {
+            return "You Win";
+        } else {
+            return "Computer Wins";
         }
-        if (playerSelection === computerSelection) {
-            return "It's a Tie";
+    }else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            return "You Win";
+        } else {
+            return "Computer Wins";
         }
-        if (playerSelection !== "rock" && playerSelection !== "scissors" && playerSelection !== "paper") {
-            return "Invalid input";
+    }else {
+        if (computerSelection === "Paper") {
+            return "You Win";
+        } else {
+            return "Computer Wins";
         }
-        if (playerSelection === "Rock") {
-            if (computerSelection === "Scissors") {
-                return "You Win";
-            } else {
-                return "Computer Wins";
-            }
-        }else if (playerSelection === "Paper") {
-            if (computerSelection === "Rock") {
-                return "You Win";
-            } else {
-                return "Computer Wins";
-            }
-        }else {
-            if (computerSelection === "Paper") {
-                return "You Win";
-            } else {
-                return "Computer Wins";
-            }
-        }
+    }
 }
     
-    function game(){
-    
-        for(let i=0; i < 5; i++){
-            const userInput = prompt("Rock, Paper, Scissors").toLowerCase();
-            if(validInput(userInput) === false) {
-                userInput = prompt('Your selection is invalid! Retry!')
-            };
-            const computerSelection = computerPlay();
-            const winner = playRound(playerSelection, computerSelection);
-            console.log(winner);
-        }
-    
+function game(){
+    for(let i=0; i < 5; i++){
+        const playerSelection = prompt("Rock, Paper, Scissors").toLowerCase();
+        // if(validInput(userInput) === false) {
+        //     userInput = prompt('Your selection is invalid! Retry!')
+        // };
+        const computerSelection = computerPlay();
+        const winner = playRound(playerSelection, computerSelection);
+        console.log(winner);
     }
-    game();
+    
+}
+game();
